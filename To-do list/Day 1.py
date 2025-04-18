a4be4c48ce269bed2c9cae83a70bf3768717c3e2
soooooo-prac <ssin86108@gmail.com>
2025-04-18 22:25:49 +0900

    Season 2 - Day 1 챌린지: To-Do List 기본 구조 만들기

---
+++ To-do list/Day 1.py
@@ -0,0 +1,22 @@
+import json
+
+todo_list = []
+TODO_FILE = "todo_data.json"
+
+while True:
+    print("📋 나의 할 일 관리 프로그램\n----------------------------\n1. 할 일 추가\n0. 종료")
+    choice = int(input("메뉴 선택: "))
+    
+    if choice == "1":
+        todo = input("할 일을 입력하세요: ").strip()
+        if todo == "":
+            print("⚠️ 공백은 입력할 수 없어요!")
+            continue
+        app = {"task": todo, "done": False}
+        todo_list.append(app)
+        print("✅ 할 일이 추가되었습니다!")
+    elif choice == "0":
+        print("👋 프로그램을 종료합니다.")
+        break
+    else:
+        print("❌ 잘못된 입력입니다.")
