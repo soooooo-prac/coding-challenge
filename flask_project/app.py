c0e2f2f1cad689e9613badd49cbcd20c1d4e8a41
soooooo-prac <ssin86108@gmail.com>
2025-04-19 23:47:47 +0900

    삭제 기능 구현

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+todo_list = []
 
 @app.route("/")
 def home():
@@ -13,7 +14,19 @@ def form():
 @app.route("/submit", methods=["POST"])
 def submit():
     username = request.form["username"]
-    return f"<h2>{username}님, 환영합니다!</h2>"
+    todo_list.append(username)
+    return redirect(url_for("show_list"))
+
+@app.route("/list")
+def show_list():
+    return render_template("list.html", items=todo_list)
+
+@app.route("/delete/<int:item_id>", methods=["POST"])
+def delete(item_id):
+    if 0 <= item_id < len(todo_list):
+        todo_list.pop(item_id)
+    return redirect(url_for("show_list"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
