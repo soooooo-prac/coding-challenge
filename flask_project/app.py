0e51b7b0a47e320f7a884b67bdc2b5a7cb2a4693
soooooo-prac <ssin86108@gmail.com>
2025-04-19 23:42:38 +0900

    Day 3: HTML 폼 + 입력 처리하기

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    username = request.form["username"]
+    return f"<h2>{username}님, 환영합니다!</h2>"
 
 if __name__ == "__main__":
     app.run(debug=True)
